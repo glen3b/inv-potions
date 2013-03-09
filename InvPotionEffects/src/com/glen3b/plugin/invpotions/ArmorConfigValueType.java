274db7164139647607318afc51dd6b3ddcf24ab4
Glen Husman <wappokickingdeer@gmail.com>
2013-03-08 18:25:39 -0800

    Steps towards a config file

---
+++ InvPotionEffects/src/com/glen3b/plugin/invpotions/ArmorConfigValueType.java
@@ -0,0 +1,27 @@
+package com.glen3b.plugin.invpotions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum ArmorConfigValueType {
+	Acknowledge,
+	Ignore,
+	NonStandard;
+	
+	public static ArmorConfigValueType getType(String configvalue){
+		if(configvalue.replaceAll("\n", "").replaceAll("\r", "") == "Wool"){
+			return ArmorConfigValueType.NonStandard;
+		}else if(configvalue.replaceAll("\n", "").replaceAll("\r", "") == "Ignore"){
+			return ArmorConfigValueType.Ignore;
+		}
+		return ArmorConfigValueType.Acknowledge;
+	}
+	
+	public static List<ArmorConfigValueType> populateList(String[] configvalues){
+		List<ArmorConfigValueType> acvt = new ArrayList<ArmorConfigValueType>();
+		for(int i = 0; i < configvalues.length; i++){
+			acvt.add(getType(configvalues[i]));
+		}
+		return acvt;
+	}
+}
--- InvPotionEffects/src/com/glen3b/plugin/invpotions/InventoryPotionEffects.java
+++ InvPotionEffects/src/com/glen3b/plugin/invpotions/InventoryPotionEffects.java
@@ -1,6 +1,14 @@
 package com.glen3b.plugin.invpotions;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.logging.Level;
+
 import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
@@ -12,84 +20,34 @@ public class InventoryPotionEffects extends JavaPlugin {
 	
 	private int taskid;
 	
-	protected boolean woolHelmet(PlayerInventory pi){
+	protected boolean nonstandardHelmet(PlayerInventory pi){
 		ItemStack h = pi.getHelmet();
 		return h != null && h.getType() != Material.LEATHER_HELMET && h.getType() != Material.GOLD_HELMET && h.getType() != Material.CHAINMAIL_HELMET && h.getType() != Material.IRON_HELMET && h.getType() != Material.DIAMOND_HELMET;
 	}
 	
 	@Override
 	public void onEnable(){
+		this.saveDefaultConfig();
 	    taskid = this.getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 	    	
 	        @Override  
 	        public void run() {
+	        	Iterator<Entry<String, Object>> nondeepconfig = getConfig().getDefaultSection().getValues(false).entrySet().iterator();
+	        	while(nondeepconfig.hasNext()){
+        			Entry<String, Object> entry = nondeepconfig.next();
+        			String basekey = entry.getKey()+".";
+        			List<ArmorConfigValueType> armorcfg = ArmorConfigValueType.populateList(new String[]{
+        					getConfig().getString(basekey+"armor.helmet"),
+        					getConfig().getString(basekey+"armor.chestplate"),
+        					getConfig().getString(basekey+"armor.leggings"),
+        					getConfig().getString(basekey+"armor.boots")});
+        			List<Material> armor = new ArrayList<Material>();
+        			armor.add(Material.getMaterial(getConfig().getString(basekey+"armor.helmet")));
+        		}
 	            for(Player p : getServer().getOnlinePlayers()) {
 	            	if(p != null){
+	            		PlayerInventory pi = p.getInventory();
 	            		
-	            		
-	            		
-	            		/*
-	            	PlayerInventory pi = p.getInventory();
-	                if(woolHelmet(pi) && pi.getBoots() != null && pi.getBoots().getType() == Material.DIAMOND_BOOTS && pi.getLeggings() != null &&pi.getLeggings().getType() == Material.DIAMOND_LEGGINGS && pi.getChestplate() != null && pi.getChestplate().getType() == Material.DIAMOND_CHESTPLATE && pi.contains(Material.DIAMOND_SWORD)){
-	                 	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-	                 	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-	                 	p.removePotionEffect(PotionEffectType.SPEED);
-	                 	p.removePotionEffect(PotionEffectType.WEAKNESS);
-	                 	p.removePotionEffect(PotionEffectType.JUMP);
-	                 	p.removePotionEffect(PotionEffectType.INVISIBILITY);
-	                	p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 1500, 3, false));
-	                 	p.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 900, 0, false));
-	                    if(r.nextInt(12) == 9){p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 250, 0, false));p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 50, 0, false));}
-	                }else if(woolHelmet(pi) && pi.contains(Material.STONE_SWORD) && pi.getBoots() == null && pi.getLeggings() == null && pi.getChestplate() != null && pi.getChestplate().getType() == Material.LEATHER_CHESTPLATE){
-	                	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-	                 	p.removePotionEffect(PotionEffectType.SLOW);
-	                 	p.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-	                 	p.removePotionEffect(PotionEffectType.WEAKNESS);
-	                 	p.removePotionEffect(PotionEffectType.JUMP);
-	                 	// p.removePotionEffect(PotionEffectType.INVISIBILITY);
-	                	p.addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, 1500, 0, false));
-	                 	p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 900, 1, false));
-	                }else if(woolHelmet(pi) && pi.contains(Material.BOW) && pi.contains(Material.IRON_SWORD) && pi.getBoots() == null && pi.getLeggings() == null && pi.getChestplate() != null && pi.getChestplate().getType() == Material.CHAINMAIL_CHESTPLATE){
-	                	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-	                 	p.removePotionEffect(PotionEffectType.SLOW);
-	                 	p.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-	                 	p.removePotionEffect(PotionEffectType.SPEED);
-	                 	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-	                 	p.removePotionEffect(PotionEffectType.INVISIBILITY);
-	                	p.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 75, 0, false));
-	                 	p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 1500, 0, false));
-	                }else if(woolHelmet(pi) && pi.contains(Material.BOW) && !pi.contains(Material.IRON_SWORD) && pi.getBoots() == null && pi.getLeggings() == null && pi.getChestplate() != null && pi.getChestplate().getType() == Material.CHAINMAIL_CHESTPLATE){
-	                	p.removePotionEffect(PotionEffectType.WEAKNESS);
-	                	p.removePotionEffect(PotionEffectType.JUMP);
-	                 	p.removePotionEffect(PotionEffectType.SLOW);
-	                 	p.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-	                 	p.removePotionEffect(PotionEffectType.SPEED);
-	                 	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-	                 	p.removePotionEffect(PotionEffectType.INVISIBILITY);
-	                	p.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 1500, 0, false));
-	                }else if(woolHelmet(pi) && pi.contains(Material.GOLD_SWORD) && pi.getBoots() != null && pi.getBoots().getType() == Material.CHAINMAIL_BOOTS && pi.getLeggings() == null && pi.getChestplate() == null){
-	                	p.removePotionEffect(PotionEffectType.WEAKNESS);
-	                	p.removePotionEffect(PotionEffectType.JUMP);
-	                 	p.removePotionEffect(PotionEffectType.SLOW);
-	                 	p.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-	                 	p.removePotionEffect(PotionEffectType.SPEED);
-	                 	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-	                 	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-	                	p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 1500, 0, false));
-	                }else if(pi.contains(Material.DIAMOND_SWORD) && !pi.contains(Material.BOW) && woolHelmet(pi) && pi.getBoots() == null && pi.getLeggings() == null && pi.getChestplate() != null && pi.getChestplate().getType() == Material.IRON_CHESTPLATE){
-	                	p.removePotionEffect(PotionEffectType.JUMP);
-	                 	p.removePotionEffect(PotionEffectType.SLOW);
-	                 	p.removePotionEffect(PotionEffectType.DAMAGE_RESISTANCE);
-	                 	p.removePotionEffect(PotionEffectType.SPEED);
-	                 	p.removePotionEffect(PotionEffectType.WATER_BREATHING);
-	                 	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-	                 	p.removePotionEffect(PotionEffectType.INVISIBILITY);
-	                	p.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 87, 0, false));
-	                }
-	                else if((p.getLocation().getBlockY() == 25 || p.getLocation().getBlockY() == 24 || p.getLocation().getBlockY() == 26) && pi.contains(Material.DIAMOND_SWORD) && pi.contains(Material.SHEARS) && pi.contains(Material.DIAMOND_SPADE) && pi.getBoots() == null && pi.getLeggings() == null && pi.getChestplate() == null && pi.getHelmet() == null){
-	                	p.removePotionEffect(PotionEffectType.NIGHT_VISION);
-	                	p.addPotionEffect((new PotionEffect(PotionEffectType.BLINDNESS, 1500, 0, false)));
-	                }*/
 	            	}
 	                
 	            }
