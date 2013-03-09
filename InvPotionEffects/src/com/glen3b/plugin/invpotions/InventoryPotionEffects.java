0a26180e9b21a6c7c3bf2f8172154f1a54bb73b2
Glen Husman <wappokickingdeer@gmail.com>
2013-03-09 08:27:17 -0800

    Beta config support

--- InvPotionEffects/src/com/glen3b/plugin/invpotions/InventoryPotionEffects.java
+++ InvPotionEffects/src/com/glen3b/plugin/invpotions/InventoryPotionEffects.java
@@ -1,21 +1,15 @@
 package com.glen3b.plugin.invpotions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.Set;
 import java.util.logging.Level;
 
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
-//import org.bukkit.potion.PotionEffect;
-//import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
@@ -55,6 +49,7 @@ public class InventoryPotionEffects extends JavaPlugin {
 	            					Material.getMaterial(getConfig().getString(basekey+"armor.boots"))};
 	            			//getLogger().log(Level.INFO, "Configuration prefix is "+basekey+", and their is a helmet of: "+getConfig().getString(basekey+"armor.helmet")+" with a material of "+armor[0]+", "+p.getDisplayName()+" has a helmet of "+parmor[3]);
 	            			boolean armorvalid = true;
+	            			boolean inventoryvalid = true;
 	            			for(int i = 0; i < 4; i++){
 		            			if(
 		            					!((armorcfg.get(i) == ArmorConfigValueType.Acknowledge && parmor[3-i].getType() == armor[i])
@@ -63,12 +58,34 @@ public class InventoryPotionEffects extends JavaPlugin {
 		            				armorvalid = false;
 		            			}
 		            			}
+	            			List<String> criteria = getConfig().getStringList(basekey+"criteria");
+	            			for(int i = 0; i < criteria.size(); i++){
+	            				Material checking = Material.getMaterial(criteria.get(i));
+		            			if(checking != null && !pi.contains(checking)){
+		            				inventoryvalid = false;
+		            			}
 		            		}
-	            		}
-	                
-	            }
-	        
-	        }
+	            			if(armorvalid && inventoryvalid){
+	            				List<String> potioneffects = getConfig().getStringList(basekey+"effects");
+	            				for(int i = 0; i < potioneffects.size(); i++){
+		            				String[] components = potioneffects.get(i).split("::");
+		            				int level;
+		            				try{
+		            				level = Integer.parseInt(components[1])-1;
+		            				}catch(NumberFormatException n){
+		            					getLogger().log(Level.WARNING, "Your potion effect configuration has an invalid number as a level.");
+		            					break;
+		            				}catch(ArrayIndexOutOfBoundsException n){
+		            					getLogger().log(Level.WARNING, "Your potion effect configuration does not have a valid splitting character ('::') in one effect configuration.");
+		            					break;
+		            				}
+		            				PotionEffectType potionefc = PotionEffectType.getByName(components[0].toUpperCase());
+		            				p.removePotionEffect(potionefc);
+		            				p.addPotionEffect(new PotionEffect(potionefc, 1500, level));
+			            		}
+	            			}
+		            		}
+	            		}}}
 	    }, 250L, 100L);
 	}
 	
